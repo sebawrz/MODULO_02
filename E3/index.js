f98114b5ddcee4e6014d6c8f69d4168f9b9aa154
Sebawrz <sebastian_wrz@outlook.com>
2024-09-17 18:29:07 -0300

    Primer commit E3

---
+++ E3/index.js
@@ -0,0 +1,124 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    imagen: "./img/muzzarella.png",
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    imagen: "./img/cebolla.png",
+  },
+
+  {
+    id: 3,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+    imagen: "./img/4quesos.png",
+  },
+
+  {
+    id: 4,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+    imagen: "./img/especial.png",
+  },
+
+  {
+    id: 5,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    imagen: "./img/anana.png",
+  },
+];
+
+
+//Get Elements
+
+const formulario = document.getElementById("formulario");
+const card = document.getElementById("card");
+
+
+//Storage Functions
+
+const save = (key, value, storageType = localStorage) => storageType.setItem(key, JSON.stringify(value))
+
+const read = (key, storageType = localStorage) => JSON.parse(storageType.getItem(key));
+
+const remove = (key, storageType = localStorage) => storageType.removeItem(key);
+
+
+//Get the Input Value, validate it + Prevent Default.
+
+const getInputValue = (event) => {
+  event.preventDefault();
+  id = formulario.elements.id.value;
+  if (id) {
+    searchPizza(id);
+  }
+  else {
+    card.innerHTML = null;
+    card.innerHTML = `<p> Por favor, recuerde que debe ingresar un ID </p>`;
+  }
+}
+
+//Search pizza by id
+
+const searchPizza = (id) => {
+  pizza = pizzas.find((pizza) => pizza.id == id)
+  if (pizza) {
+    renderPizza(pizza)
+  }
+  else {
+    renderError(id);
+  }
+}
+
+//Render pizza card
+
+const renderPizza = (pizza) => {
+  card.innerHTML = null;
+
+  card.innerHTML = `<picture><img src="${pizza.imagen}" alt="${pizza.nombre}"></picture>
+  <h1>${pizza.nombre}</h1>
+  <h2>$ ${pizza.precio}</h2>
+  <p>${pizza.ingredientes} </p>`;
+  save("ultimaPizza", pizza, localStorage);
+}
+
+//Render error in card
+
+const renderError = (id) => {
+  card.innerHTML = null;
+  card.innerHTML = `<p>Lo siento. No existen pizza con ID ${id}.</p> <p>Pruebe con otro ID. </p>`;
+  remove("ultimaPizza", localStorage);
+}
+
+//Initialization
+
+if (read("ultimaPizza", localStorage)) {
+  renderPizza(read("ultimaPizza", localStorage));
+}
+else {
+  card.innerHTML = null;
+  card.innerHTML = `<p> Por favor, ingrese un ID </p>`;
+}
+
+
+//Event Listeners
+
+formulario.addEventListener("submit", getInputValue);
